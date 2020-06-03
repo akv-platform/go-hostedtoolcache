a6de2ccec09ac1b194a9e6e0632d654511e9787e
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-06-03 15:03:08 +0300

    add debug line

--- tests/Go.Tests.ps1
+++ tests/Go.Tests.ps1
@@ -24,11 +24,12 @@ Describe "Go" {
 
     Set-Location -Path "source"
     $sourceLocation = Get-Location
+    Write-Host "GOROOT ${GOROOT}"
 
     It "Run simple code" {
         $simpleLocation = Join-Path -Path $sourceLocation -ChildPath "simple"
         Set-Location -Path $simpleLocation
-        go mod init example.com/user/hello
+        go mod init example.com/m
         "go run simple.go" | Should -ReturnZeroExitCode
         "go build simple.go" | Should -ReturnZeroExitCode
         "./simple" | Should -ReturnZeroExitCode
@@ -37,7 +38,7 @@ Describe "Go" {
     It "Run maps code" {
         $mapsLocation = Join-Path -Path $sourceLocation -ChildPath "maps"
         Set-Location -Path $mapsLocation
-        go mod init example.com/user/hello
+        go mod init example.com/m
         "go run maps.go" | Should -ReturnZeroExitCode
         "go build maps.go" | Should -ReturnZeroExitCode
         "./maps" | Should -ReturnZeroExitCode
@@ -46,7 +47,7 @@ Describe "Go" {
     It "Run methods code" {
         $methodsLocation = Join-Path -Path $sourceLocation -ChildPath "methods"
         Set-Location -Path $methodsLocation
-        go mod init example.com/user/hello
+        go mod init example.com/m
         "go run methods.go" | Should -ReturnZeroExitCode
         "go build methods.go" | Should -ReturnZeroExitCode
         "./methods" | Should -ReturnZeroExitCode
