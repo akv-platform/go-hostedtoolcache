e73d2701b38edd02e6ce272ad32377e6cb735d60
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-06-03 16:45:32 +0300

    some clean up

--- tests/Go.Tests.ps1
+++ tests/Go.Tests.ps1
@@ -26,11 +26,10 @@ Describe "Go" {
     $sourceLocation = Get-Location
     Write-Host "GOROOT ${env:GOROOT}"
     Write-Host "PATH ${env:PATH}"
-    go env
+    "go mod init example.com/m" | Should -ReturnZeroExitCode
     It "Run simple code" {
         $simpleLocation = Join-Path -Path $sourceLocation -ChildPath "simple"
         Set-Location -Path $simpleLocation
-        go mod init example.com/m 2>&1 | Out-Null
         "go run simple.go" | Should -ReturnZeroExitCode
         "go build simple.go" | Should -ReturnZeroExitCode
         "./simple" | Should -ReturnZeroExitCode
@@ -39,7 +38,6 @@ Describe "Go" {
     It "Run maps code" {
         $mapsLocation = Join-Path -Path $sourceLocation -ChildPath "maps"
         Set-Location -Path $mapsLocation
-        go mod init example.com/m 2>&1 | Out-Null
         "go run maps.go" | Should -ReturnZeroExitCode
         "go build maps.go" | Should -ReturnZeroExitCode
         "./maps" | Should -ReturnZeroExitCode
@@ -48,7 +46,6 @@ Describe "Go" {
     It "Run methods code" {
         $methodsLocation = Join-Path -Path $sourceLocation -ChildPath "methods"
         Set-Location -Path $methodsLocation
-        go mod init example.com/m 2>&1 | Out-Null
         "go run methods.go" | Should -ReturnZeroExitCode
         "go build methods.go" | Should -ReturnZeroExitCode
         "./methods" | Should -ReturnZeroExitCode
