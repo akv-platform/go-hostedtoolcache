2edc259f6112317ffcc92e0cf29a6cbf8a9f6320
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-06-03 14:40:57 +0300

    try new test code

--- tests/Go.Tests.ps1
+++ tests/Go.Tests.ps1
@@ -22,15 +22,33 @@ Describe "Go" {
         $goPath.startsWith($expectedPath) | Should -BeTrue -Because "'$goPath' is not started with '$expectedPath'"
     }
 
+    Set-Location -Path "source"
+    $sourceLocation = Get-Location
+
     It "Run simple code" {
-        Write-Host "GOROOT $env:GOROOT"
-        Write-Host "GOPATH $env:GOPATH"
-        Write-Host "GOBIN $env:GOBIN"
-        Set-Location -Path "simple"
-        Write-Host "GOROOT $env:GOROOT"
-        Write-Host "GOPATH $env:GOPATH"
-        Write-Host "GOBIN $env:GOBIN"
-        "go run simple" | Should -ReturnZeroExitCode
-        "simple" | Should -ReturnZeroExitCode
+        $simpleLocation = Join-Path -Path $sourceLocation -ChildPath "simple"
+        Set-Location -Path $simpleLocation
+        go mod init example.com/user/hello
+        "go run simple.go" | Should -ReturnZeroExitCode
+        "go build simple.go" | Should -ReturnZeroExitCode
+        "./simple" | Should -ReturnZeroExitCode
+    }
+
+    It "Run maps code" {
+        $mapsLocation = Join-Path -Path $sourceLocation -ChildPath "maps"
+        Set-Location -Path $mapsLocation
+        go mod init example.com/user/hello
+        "go run maps.go" | Should -ReturnZeroExitCode
+        "go build maps.go" | Should -ReturnZeroExitCode
+        "./maps" | Should -ReturnZeroExitCode
+    }
+
+    It "Run methods code" {
+        $methodsLocation = Join-Path -Path $sourceLocation -ChildPath "methods"
+        Set-Location -Path $methodsLocation
+        go mod init example.com/user/hello
+        "go run methods.go" | Should -ReturnZeroExitCode
+        "go build methods.go" | Should -ReturnZeroExitCode
+        "./methods" | Should -ReturnZeroExitCode
     }
 }
